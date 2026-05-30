f099a3d602dfa1fbe9cb67f5653fef5e052ba724
Nguyen Thi Yen Nhi <nguyenthiyennhi080.604@gmail.com>
2026-05-30 17:52:28 +0700

    Update HRM project

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,26 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  userRole = '';
 
   constructor(
     private router: Router
-  ){}
+  ) {}
 
-  logout(){
+  ngOnInit(): void {
+
+    const token = localStorage.getItem('token');
+
+    if (token) {
+
+      const decoded: any = jwtDecode(token);
+
+      this.userRole =
+        decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+      console.log('Role:', this.userRole);
+    }
+  }
+
+  logout() {
 
     localStorage.removeItem('token');
 
     this.router.navigate(['/login']);
 
   }
-
 }
